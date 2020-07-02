63d858184cca1e67115497bc7e9e3ac47b8296d9
PyCaret <moez@pycaret.org>
2020-07-02 16:11:56 -0400

    Add files via upload

---
+++ train.py
@@ -0,0 +1,13 @@
+#loading dataset
+from pycaret.datasets import get_data
+data = get_data('juice')
+
+#init setup
+from pycaret.classification import setup, create_model, finalize_model
+clf1 = setup(data, target = 'Purchase', logging=True, session_id=123, experiment_name='juice-mlflow-1', silent=True)
+
+#training rf
+rf = create_model('rf')
+
+#finalize rf
+final_rf = finalize_model(rf)
