055ae49fb115905e3ed644c82d5d2ce913e18718
PyCaret <moez@pycaret.org>
2020-07-02 16:37:39 -0400

    Update train.py

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ data = get_data('juice')
 
 #init setup
 from pycaret.classification import setup, create_model, finalize_model
-clf1 = setup(data, target = 'Purchase', logging=True, session_id=123, experiment_name='juice-mlflow-1', silent=True)
+clf1 = setup(data, target = 'Purchase', logging=True, session_id=123, experiment_name='juice-mlflow-1', silent=True, html=False)
 
 #training rf
 rf = create_model('rf')
