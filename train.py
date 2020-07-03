7d3e998005b462a838526f78a408ede887772cb9
PyCaret <moez@pycaret.org>
2020-07-02 21:59:57 -0400

    Update train.py

--- train.py
+++ train.py
@@ -1,10 +1,14 @@
+#getting experiment name
+import sys
+exp_param = sys.argv[0]
+
 #loading dataset
 from pycaret.datasets import get_data
 data = get_data('juice')
 
 #init setup
 from pycaret.classification import setup, create_model, finalize_model
-clf1 = setup(data, target = 'Purchase', logging=True, session_id=123, silent=True, html=False)
+clf1 = setup(data, target = 'Purchase', logging=True, experiment_name = exp_param, session_id=123, silent=True, html=False)
 
 #training rf
 rf = create_model('rf')
