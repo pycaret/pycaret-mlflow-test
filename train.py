d16438b5363da87a3955b64ac64a88776ada7538
PyCaret <moez@pycaret.org>
2020-07-02 21:29:49 -0400

    Update train.py

--- train.py
+++ train.py
@@ -1,18 +1,10 @@
-#experiment_name
-import sys
-exp_name = sys.argv[0]
-
-#setting experiment name
-import mlflow
-mlflow.set_experiment(exp_name)
-
 #loading dataset
 from pycaret.datasets import get_data
 data = get_data('juice')
 
 #init setup
 from pycaret.classification import setup, create_model, finalize_model
-clf1 = setup(data, target = 'Purchase', logging=True, session_id=123, experiment_name=exp_name, silent=True, html=False)
+clf1 = setup(data, target = 'Purchase', logging=True, session_id=123, experiment_name='banana1', silent=True, html=False)
 
 #training rf
 rf = create_model('rf')
